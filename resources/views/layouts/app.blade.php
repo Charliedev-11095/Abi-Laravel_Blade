3c92d982bd9efb5cf142d71e478aac562567d394
Charliedev-11095 <carlosroyiga.com>
2021-09-08 11:46:12 -0500

    icono pelota basquet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <link rel="icon" href="{{ url('css/basketball.png') }}">
+        <link rel="shortcut icon" type="image/png" href="{{ asset('basketball.png') }}"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
