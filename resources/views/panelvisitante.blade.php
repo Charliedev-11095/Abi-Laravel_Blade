ff629374463ab8cd49c6c7e04dac584ff5d3daff
NeroArt <68074796+NeroArt@users.noreply.github.com>
2021-09-11 20:06:32 -0500

    panel visitante terminado

--- resources/views/panelvisitante.blade.php
+++ resources/views/panelvisitante.blade.php
@@ -1,7 +1,101 @@
 
 <x-app-layout>
 
-Bienvenido a pagina de VISITANTE
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!DOCTYPE HTML>
+    <html>
+        <head>
+        
+        <title>perfil</title>
+        
+        <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,700,800" rel="stylesheet">
+        
+    
+        </head>
+        <body>
+            
+        <header id="fh5co-header" class="" role="banner" style="background-image:url({{ asset('images/img5.jpg') }});" data-stellar-background-ratio="0.5">
+                
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-8 col-md-offset-2 text-center fh5co-heading">
+                        <div class="display-t">
+                            <div class="display-tc animate-box" data-animate-effect="fadeIn">
+                                <h1 style="color:rgb(255, 255, 255);"><br> <strong>BIENVENIDO VISITANTE</strong></h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="page-wrapper bg-gra-03 p-t-45 p-b-50">
 
+
+
+        <div id="fh5co-services" class="fh5co-bg-section">
+            
+           
+            
+            <div class="container">
+                
+                <div class="card card-5">
+                    <div class="card-heading">
+                                        
+                    <H2 class="title">Información</H2>
+                   
+                    </div>
+                    <div class="card-body">
+
+                   
+                 
+                        <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                           <center><img src="{{ asset('images/imgvisit.png') }}" ></center> 
+                         </div>
+                        
+
+                        <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                           <h3>La pagina web es de uso exclusivo para usuarios y clubs autorizados.</h3> 
+                        </div>
+
+                        <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                            <p>
+                                <h3>
+                                Si te interesa mantenerte al tanto y pedir informacion, comunícate con nosotros a través de <a href="https://www.facebook.com/Academia-Basquetbol-Integral-117194377233584"><h3>Facebook</h3></a> y visita nuestra página de contacto.
+                                </h3>
+                            </p>
+                                
+                            
+                             
+                        </div>
+                        <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                           <h3>Comencemos a trabajar juntos!!!!
+                            
+                            </h3> 
+                         
+                        </div>
+                        
+                     
+
+
+                    </div>
+                </div>
+
+
+
+
+                <br>
+           
+                
+               
+
+                <div class="gototop js-top">
+                    <a href="#" class="js-gotop"><i class="icon-arrow-up"><br>subir</i></a>
+                </div>
+            </div>
+        </div>
+        
+        </div>
+        </body>
+    </html>
 
 </x-app-layout>
