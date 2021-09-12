8c420624b7d4498ab0cb77c90d50cd10b6ccbc84
Charliedev-11095 <carlosroyiga.com>
2021-09-11 19:06:46 -0500

    welcome corregido

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -145,7 +145,7 @@
 
     </body>
 </html>
-<div class="relative flex items-top justify-center min-h-screen sm:items-center py-4 sm:pt-0">
+<div class="absolute flex items-top justify-center min-h-screen sm:items-center py-4 sm:pt-0">
     @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
