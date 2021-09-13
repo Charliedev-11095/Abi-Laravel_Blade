81cee2fcf85acb6d0fa7bb303d877c2105256a2f
Charliedev-11095 <carlosroyiga.com>
2021-09-13 17:45:01 -0500

    pdf form end

--- app/Http/Controllers/PDFcontroller.php
+++ app/Http/Controllers/PDFcontroller.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use PDF;
-
 use App\Models\alumnos;
 use App\Models\tutores;
 use App\Models\entrenadores;
 use App\Models\registros_medicos;
+use DB;
 
 use Illuminate\Http\Request;
 
@@ -39,7 +39,12 @@ class PDFcontroller extends Controller
 
    }
    public function reg_medPDF(){
-      $formreg_med=registros_medicos::all();
+      // $formreg_med=registros_medicos::all();
+      // $alumnos = alumnos::all();
+      $formreg_med=DB::table('registros_medicos')
+      ->join('alumnos','alumnos.id', '=','registros_medicos.alumnos_id')
+      ->select('registros_medicos.*','alumnos.nombres','alumnos.apellido_paterno','alumnos.apellido_materno')
+      ->get();
       $pdf = PDF::loadView('formreg_med.reg_medPDF',compact('formreg_med'));
       return $pdf->setPAper('a4','portrait')->stream('Reporte-entrenador.pdf');
 
