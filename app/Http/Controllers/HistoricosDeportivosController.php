56a0347af5e4cfad694c75d462d581c895c2b638
NeroArt <pacto.polar88@gmail.com>
2021-10-05 01:42:19 -0500

    fix evaluaciones_maximo en grupos

--- app/Http/Controllers/HistoricosDeportivosController.php
+++ app/Http/Controllers/HistoricosDeportivosController.php
@@ -97,50 +97,78 @@ class HistoricosDeportivosController extends Controller
            $valorid = $a->id;
        }
 
+       //Se insertan los datos recuperados(incluyendo el id de grupo_alumnos para la columna: relacion_grupo_alumnos)
+       $datosGrupo=[
+        'comunicacion'=>request('comunicacion'),
+        'liderazgo'=>request('liderazgo'),
+        'respeto'=>request('respeto'),
+        'responsabilidad'=>request('responsabilidad'),
+        'participacion'=>request('participacion'),
+        'actitud'=>request('actitud'),
+        'constancia'=>request('constancia'),
+        'compromiso'=>request('compromiso'),
+        'trabajo_en_equipo'=>request('trabajo_en_equipo'),
+
+        'mirada_al_frente'=>request('mirada_al_frente'),
+        'coordinacion_manos_balon'=>request('coordinacion_manos_balon'),
+        'decision_bajo_presion'=>request('decision_bajo_presion'),
+        'acertividad_en_balon'=>request('acertividad_en_balon'),
+        'coordinacion_manos_pase'=>request('coordinacion_manos_pase'),
+        'rapidez_en_pase'=>request('rapidez_en_pase'),
+        'pase_al_poste'=>request('pase_al_poste'),
+        'acertividad_en_pase'=>request('acertividad_en_pase'),
+        'balance_pies'=>request('balance_pies'),
+        'pivote'=>request('pivote'),
+        
+        'balance_objetivo'=>request('balance_objetivo'),
+        'agarre_balon'=>request('agarre_balon'),
+        'alineacion_al_aro'=>request('alineacion_al_aro'),
+        'entradas_manos'=>request('entradas_manos'),
+        'posicion_cuerpo'=>request('posicion_cuerpo'),
+        'presion_balon'=>request('presion_balon'),
+        'bloqueo_oponente'=>request('bloqueo_oponente'),
+        'contesta_lanzamiento'=>request('contesta_lanzamiento'),
+        'observaciones'=>request('observaciones'),
+        'alumnos_id'=>request('alumnos_id'),
+        'relacion_grupo_alumnos'=>$valorid,
+   
+    ];
+
+       historicos_deportivos::insert($datosGrupo);
+
+//Se encuentran los ids de los alumnos pertenecientes a X grupo
+       $datosalumnos =DB::table('grupo_alumnos')
+       ->where('grupo_alumnos.grupos_id', '=', $nombregrupo)
+       ->select('grupo_alumnos.id as idalumnos')
+       ->get(array('idalumnos'));
 
 
-       // $datosGrupo=request()->except('_token');
+//Declaramos un contador, para que lleve la cuenta de trabajos, y otra para el id de la relacion_grupo_alumno
+$valormaximo = 0;
+$valorb = '';
+
+
+//Se recorren los alumnos pertenecientes al grupo, se encuentra quien tiene mas trabajos evaluados
+// y el maximo se actualiza en la columna de grupos correspondiente
+ foreach ($datosalumnos as $datoalumno) {
+    $valorb = $datoalumno->idalumnos;
+
+    $contador_trabajos = DB::table('historicos_deportivos')
+    ->where('historicos_deportivos.relacion_grupo_alumnos', '=', $valorb)
+    ->count();
+
+    if ($contador_trabajos >=$valormaximo) {
+        $valormaximo = $contador_trabajos;
         $datosGrupo=[
-            'comunicacion'=>'required|string|max:10',
-            'liderazgo'=>'required|string|max:10',
-            'respeto'=>'required|string|max:10',
-            'responsabilidad'=>'required|string|max:10',
-            'participacion'=>'required|string|max:10',
-            'actitud'=>'required|string|max:10',
-            'constancia'=>'required|string|max:10',
-            'compromiso'=>'required|string|max:10',
-            'trabajo_en_equipo'=>'required|string|max:10',
-
-            'mirada_al_frente'=>'required|string|max:10',
-            'coordinacion_manos_balon'=>'required|string|max:10',
-            'decision_bajo_presion'=>'required|string|max:10',
-            'acertividad_en_balon'=>'required|string|max:10',
-            'coordinacion_manos_pase'=>'required|string|max:10',
-            'rapidez_en_pase'=>'required|string|max:10',
-            'pase_al_poste'=>'required|string|max:10',
-            'acertividad_en_pase'=>'required|string|max:10',
-            'balance_pies'=>'required|string|max:10',
-            'pivote'=>'required|string|max:10',
-            
-            'balance_objetivo'=>'required|string|max:10',
-            'agarre_balon'=>'required|string|max:10',
-            'alineacion_al_aro'=>'required|string|max:10',
-            'entradas_manos'=>'required|string|max:10',
-            'posicion_cuerpo'=>'required|string|max:10',
-            'presion_balon'=>'required|string|max:10',
-            'bloqueo_oponente'=>'required|string|max:10',
-            'contesta_lanzamiento'=>'required|string|max:10',
-            'observaciones'=>'required|string|max:100',
-            'alumnos_id'=>'required|integer',
-            'relacion_grupo_alumnos'=>$valorid,
-       
+            'evaluaciones_maximo'=>$valormaximo,
         ];
+         Grupos::where('id','=',$nombregrupo)->update($datosGrupo);
 
+    }
 
+ }
 
-         historicos_deportivos::insert($datosGrupo);
-     
-         return redirect('dashboard')->with('Mensaje','Historico Deportivo agregado con exito');
+         return redirect('dashboard')->with('Mensaje','Histórico Deportivo agregado con éxito');
     }
 
     /**
@@ -239,7 +267,7 @@ class HistoricosDeportivosController extends Controller
         //         (nombrecarpeta.nombrevista)
         // COMENTADA return view('asistencia.editgrupo',compact('grupo'));
 
-        return redirect('formhistorico_deportivo')->with('Mensaje','Registro Historico Deportivo modificado con exito');
+        return redirect('formhistorico_deportivo')->with('Mensaje','Histórico Deportivo modificado con éxito');
     
     }
 
